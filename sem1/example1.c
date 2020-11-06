a5ec5a7f676a39a7f7271a3e5ea7859ad1bf6757
Igor Mineev <>
2020-11-06 14:26:34 +0300

    Sem1

--- sem1/example1.c
+++ sem1/example1.c
@@ -1,10 +1,28 @@
 #include "stdio.h"
+#include "stdlib.h"
 
 // int main() {
 // int main(int argc, const char * argv[]) {
 int main(int argc, char ** argv) {
     int x;
-    scanf("%d", &x);
+    long y;
+    int scanned = scanf("%d %ld", &x, &y);
+    if (scanned != 2) {
+        return 1; /// 1llu 1l 1ll ...
+        // exit(1);
+    }
 
-    printf("%d", x);
+    printf("%d %d %ld\n", scanned, x, y);
+
+    int c_in;
+    // c_in = getc(stdin);
+    while ((c_in = getc(stdin)) != EOF) { // c_in != EOF
+        char c = (char)c_in;
+        /// ...
+    }
+
+    float f;
+    double d;
+
+    return 0;
 }
