3a41b375af40353c4b457aafb32045cfd8e3788e
Igor Mineev <>
2021-02-05 14:35:20 +0300

    Add sem11 fix.

--- sem11/example2.s
+++ sem11/example2.s
@@ -2,7 +2,21 @@
         .text
         .global _start
 
+
+process:
+
+        movl   $__NR_read, %eax
+        movl   $0, %ebx
+        movl   A, %ecx
+        movl   1, %edx
+        int    $0x80
+        ret
+
 _start:
+        call process
         movl   $__NR_exit, %eax
         movl   $3, %ebx
         int    $0x80
+
+        .data
+A:      .int   0
